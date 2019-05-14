8e26f5c7b0ec8659174bde4ee74a0f636738538b
Mayank Pandey <pandeyz@yahoo.in>
2019-05-14 15:36:50 +0530

    Add files via upload

--- api.php
+++ api.php
@@ -17,5 +17,8 @@ Route::post('projects', 'ProjectController@store');
 Route::get('projects/{id}', 'ProjectController@show');
 Route::put('projects/{project}', 'ProjectController@markAsCompleted');
 
+// Delete project
+Route::post('projects/{id}', 'ProjectController@delete');
+
 Route::post('tasks', 'TaskController@store');
 Route::put('tasks/{task}', 'TaskController@markAsCompleted');
